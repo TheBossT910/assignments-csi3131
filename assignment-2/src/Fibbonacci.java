05996f4203f0fcfc08489fb7ed3e25b99778600b
Samuel Leone-Ganado <sleon091@uottawa.ca>
2026-05-30 12:48:27 -0400

    Added description and names

--- assignment-2/src/Fibbonacci.java
+++ assignment-2/src/Fibbonacci.java
@@ -1,3 +1,18 @@
+/* ------------------------------------------------ ------------
+File: Fibbonacci.java
+
+Partner 1
+Last name: Rashid, Taha
+Student number: 300403833
+
+Partner 2
+Last name: Leone-Ganado, Samuel
+Student number: 300405286
+
+Description: This program prints the first n values of the fibonacci
+	sequence, n being the number privvded on run
+------------------------------------------------------------- */
+
 import java.util.Scanner;
 
 public class Fibbonacci {
--- assignment-2/src/Prime.java
+++ assignment-2/src/Prime.java
@@ -1,3 +1,18 @@
+/* ------------------------------------------------ ------------
+File: Prime.java
+
+Partner 1
+Last name: Rashid, Taha
+Student number: 300403833
+
+Partner 2
+Last name: Leone-Ganado, Samuel
+Student number: 300405286
+
+Description: This program prints prime values up to n
+	n being the number provided by user on run
+------------------------------------------------------------- */
+
 import java.util.Scanner;
 
 public class Prime {
